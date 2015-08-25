fd23d7ffbe3eeaa6abbb0b547400e9009a3747ce
Pancho <lonchbox@gmail.com>
2015-08-25 14:06:20 +0200

    change text

--- inc/pointers.php
+++ inc/pointers.php
@@ -7,7 +7,7 @@ function ttt_social_HelpPointers() {
 			'screen' => 'plugins', // this is the page hook we want our pointer to show on
 			'target' => '#menu-settings', // the css selector for the pointer to be tied to, best to use ID's
 			'title' => __('1º Connect your Social Profiles','ttt_social'),
-			'content' => __('Go to <a href="'.admin_url( 'options-general.php?page=ttt-social-menu' ).'">Settings -> TTT Social</a>. Just need to make an OAuth connection with your Twitter account and Instagram. Also can change APP Key and Secret for Facebook, Instagram and Twittter.', 'ttt_social'),
+			'content' => __('Go to <a href="'.admin_url( 'options-general.php?page=ttt-social-menu' ).'">Settings -> TTT Social</a>. Just need to make an OAuth connection with your Twitter account and Instagram.', 'ttt_social'),
 			'position' => array( 
 				'edge' => 'left', //top, bottom, left, right
 				'align' => 'middle' //top, bottom, left, right, middle
